77f931a94bd1f99af6d2a60a6a6a1dbfd9bf34fc
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-23 02:36:56 +0530

    PerfectNumber

---
+++ PerfectNumber.cs
@@ -0,0 +1,24 @@
+﻿
+// 2. Perfect Number
+
+int num;
+int i = 1, 
+int total = 0;
+
+ Console.WriteLine("Enter a number");
+
+  num = int.Parse(Console.ReadLine());
+
+ for(i=1; i<num; i++)
+   {
+    if(num % i == 0)
+     total += i;
+   }
+   if(total == num)
+   {
+    Console.Write("{0} is a perfect number", num);
+   }
+   else
+   {
+    Console.Write("{0} is not a perfect number", num);
+	}
