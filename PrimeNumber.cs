df3ab22f2d3803603367bd41116710fc6bd02165
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-23 02:40:20 +0530

    PrimeNumber

---
+++ PrimeNumber.cs
@@ -0,0 +1,20 @@
+﻿int num; int count = 0;
+		Console.WriteLine("Number");
+		num = int.Parse(Console.ReadLine());
+		
+    for ( int i = 1; i <=num; i++)
+	{
+	   if (num % i == 0)
+	  {
+	count++;
+	  }
+	}
+		if(count == 2)
+		{
+		Console.WriteLine("prime Number");
+	}	
+	
+		else 
+		{
+		Console.WriteLine("Not a prime Number");
+		}
