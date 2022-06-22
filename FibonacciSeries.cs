66d31743d93105e75f28ce9897bcff4995c5f774
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-23 02:29:20 +0530

    FibonacciSeries

---
+++ FibonacciSeries.cs
@@ -0,0 +1,17 @@
+﻿int a = 0; 
+int b = 1; 
+int c;
+		
+Console.WriteLine(a);
+		
+Console.WriteLine(b);
+		
+for( int i = 0; i< 12; i++ )	
+	    
+	c = a + b;
+		
+Console.Write(c+" ");
+		
+a = b;
+		
+b = c;
