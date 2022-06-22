b4d44a5b1ce5324587ad87ac02667b5778e3628a
Nitish Mehta <nkrmehta09@gmail.com>
2022-06-23 02:12:19 +0530

    LogicalProgram Welcome

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Logical Programs
+
+string Welcome = "Welcome to the Day 6 practice section : ";
+
+Console.WriteLine(Welcome + " Logical Programs");
